1238cc7972a10ab1699dab48c914ee9f0c087d38
Karly Mareka <karly.mareka@gmail.com>
2019-01-26 22:00:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,12 +34,12 @@ class Owner
   end 
 
   def pets
-    @@pets = Hash[:fishes => [], :dogs => [], :cats => []] 
+    @pets = Hash[:fishes => [], :dogs => [], :cats => []] 
   end
   
   def buy_fish(fish_name)
-    @@pets[:fishes] << Fish.new(fish_name)
-    @@pets
+    @pets[:fishes] << Fish.new(fish_name)
+    @pets
     binding.pry 
   end 
   
